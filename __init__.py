2209bc783349aa24a8e586bb17bcfed5489faddd
Jian He <1217875251@qq.com>
2022-09-07 22:17:50 +0800

    Fix

--- __init__.py
+++ __init__.py
@@ -4,5 +4,6 @@
 # Copyright (C) 2008-2008 凯源吕鑫 lvxin@gmail.com   <basic chart data>
 #                         维智众源 oldrev@gmail.com  <states data>
 # Copyright (C) 2012-2012 南京盈通 ccdos@intoerp.com <small business chart>
-# Copyright (C) 2008-now  开阖软件 jeff@osbzr.com    < PM and LTS >
-# Copyright (C) 2017-now  jeffery9@gmail.com
+# Copyright (C) 2008-2019  开阖软件 jeff@osbzr.com    < PM and LTS >
+# Copyright (C) 2017-2019  jeffery9@gmail.com
+# Copyright (C) 2019-now  He Jian 1217875253@gmail.com
