f5ab5d5adc167d43e131dc50295a79578c309298
hej <hej@eabax.com>
2022-09-16 17:12:20 +0800

    Fix

--- models/chart_template.py
+++ models/chart_template.py
@@ -15,17 +15,23 @@ class AccountAccountTemplate(models.Model):
 class AccountChartTemplate(models.Model):
     _inherit = 'account.chart.template'
 
-    # 科目代码不需要补齐到6位
     def generate_account(self, tax_template_ref, acc_template_ref, code_digits, company):
         self.ensure_one()
         account_tmpl_obj = self.env['account.account.template']
         acc_template = account_tmpl_obj.search([('nocreate', '!=', True), ('chart_template_id', '=', self.id)], order='id')
         template_vals = []
         for account_template in acc_template:
+            # 科目代码不需要补齐到6位
             code_acc = account_template.code or ''
             vals = self._get_account_vals(company, account_template, code_acc, tax_template_ref)
             template_vals.append((account_template, vals))
         accounts = self._create_records_with_xmlid('account.account', template_vals, company)
         for template, account in zip(acc_template, accounts):
             acc_template_ref[template.id] = account.id
+
+        # 更新 parent_id
+        for template, account in zip(acc_template, accounts):
+            if template.parent_id:
+                account.update({'parent_id': acc_template_ref[template.parent_id.id]})
+
         return acc_template_ref
