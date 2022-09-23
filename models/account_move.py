b53297c66e96b01b6ea0b4029e19dbb11b98b66f
hej <hej@eabax.com>
2022-09-23 13:34:48 +0800

    Fix

--- models/account_move.py
+++ models/account_move.py
@@ -6,5 +6,9 @@ from odoo import fields, models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
+    account_id = fields.Many2one(
+        domain="[('deprecated', '=', False), ('company_id', '=', 'company_id'),('is_off_balance', '=', False),('bend', '=', True)]",
+        )
+
     parent_account = fields.Many2one(string='Parent Account', related='account_id.parent_id', store=True)
     
