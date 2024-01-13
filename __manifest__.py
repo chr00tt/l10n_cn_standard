5dfbc7e40152461a2dfe379a2e5e28aa5b9db860
He Jian <hej@eabax.com>
2024-01-14 00:33:31 +0800

    fix

--- __manifest__.py
+++ __manifest__.py
@@ -5,8 +5,6 @@
     'name': '中国 - 企业会计准则',
     'version': '2.0',
     'category': 'Accounting/Localizations/Account Charts',
-    'author': ['lvxin@gmail.co', 'oldrev@gmail.co', 'ccdos@intoerp.com', 'jeff@osbzr.com', 'jeffery9@gmail.com', '1217875253@qq.com'],
-
     'website': 'https://github.com/chr00tt/l10n_cn_standard',
     'description': """
 Including the following data in the Accounting Standards for Business Enterprises
@@ -29,7 +27,6 @@ Including the following data in the Accounting Standards for Business Enterprise
     'data': [
         'data/l10n_cn_standard.xml',
         'data/account.account.template.csv',
-        'data/account_tax_template_data.xml',
         'data/l10n_cn_standard_post.xml',
     ],
     'license': 'LGPL-3',
