76b4a9ef03c926e6ed50934fde2f7ec4755fa706
He Jian <hej@eabax.com>
2024-04-02 10:17:18 +0800

    fix

--- __manifest__.py
+++ __manifest__.py
@@ -3,23 +3,9 @@
 
 {
     'name': '中国 - 企业会计准则',
-    'version': '2.0',
-    'category': 'Accounting/Localizations/Account Charts',
     'website': 'https://github.com/chr00tt/l10n_cn_standard',
-    'description': """
-Including the following data in the Accounting Standards for Business Enterprises
-包含企业会计准则以下数据
-
-* Chart of Accounts
-* 科目表模板
-
-* Account templates
-* 科目模板
-
-* Tax templates
-* 税金模板
-
-    """,
+    'author': "He Jian",
+    'category': 'Accounting/Localizations/Account Charts',
     'depends': [
         'account',
         "l10n_multilang",
